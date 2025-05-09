c3a19c4174db1c7f4bb5fa733570b6d837a25719
孤风雪影 <efairy520@qq.com>
2025-05-09 14:59:47 +0800

    以太网驱动封装

--- src/xnet_app/port_pcap.c
+++ src/xnet_app/port_pcap.c
@@ -1,24 +1,32 @@
 ﻿/**
- * 协议栈 ↔ 网卡 驱动接口适配层
+ * 协议栈 ↔ 网卡 适配层
  */
 #include <string.h>
 #include <stdlib.h>
 #include "pcap_device.h"
 #include "xnet_tiny.h"
 
-static pcap_t * pcap;
+static pcap_t *pcap;
 
-// pcap所用的网卡，物理网卡
-const char * ip_str = "192.168.254.1";      // 根据实际电脑上存在的网卡地址进行修改
+/**
+ * pcap 所用的真实网卡，物理网卡
+ * 一般为主机 ip 地址
+ */
+const char *ip_str = "192.168.254.1";
+
+/**
+ * 协议栈 mac 地址
+ *
+ */
 const char my_mac_addr[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
 /**
  * 初始化网络驱动
  * @return 0成功，其它失败
  */
-xnet_err_t xnet_driver_open (uint8_t * mac_addr) {
+xnet_err_t xnet_driver_open(uint8_t *mac_addr) {
     memcpy(mac_addr, my_mac_addr, sizeof(my_mac_addr));
     pcap = pcap_device_open(ip_str, mac_addr, 1);
-    if (pcap == (pcap_t *)0) {
+    if (pcap == (pcap_t *) 0) {
         exit(-1);
     }
     return XNET_ERR_OK;
@@ -30,7 +38,7 @@ xnet_err_t xnet_driver_open (uint8_t * mac_addr) {
  * @param size 数据长度
  * @return 0 - 成功，其它失败
  */
-xnet_err_t xnet_driver_send (xnet_packet_t * packet) {
+xnet_err_t xnet_driver_send(xnet_packet_t *packet) {
     return pcap_device_send(pcap, packet->data, packet->size);
 }
 
@@ -40,9 +48,9 @@ xnet_err_t xnet_driver_send (xnet_packet_t * packet) {
  * @param size 数据长度
  * @return 0 - 成功，其它失败
  */
-xnet_err_t xnet_driver_read (xnet_packet_t ** packet) {
+xnet_err_t xnet_driver_read(xnet_packet_t **packet) {
     uint16_t size;
-    xnet_packet_t * r_packet = xnet_alloc_for_read(XNET_CFG_PACKET_MAX_SIZE);
+    xnet_packet_t *r_packet = xnet_alloc_for_read(XNET_CFG_PACKET_MAX_SIZE);
 
     size = pcap_device_read(pcap, r_packet->data, XNET_CFG_PACKET_MAX_SIZE);
     if (size) {
